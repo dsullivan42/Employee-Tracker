dbf9e9e485f7217988cf72d5fbd5ac86d65ab75b
Daniel Sullivan <ojojoj89@gmail.com>
2023-10-24 09:25:18 -0500

    added the starting prompt

--- server.js
+++ server.js
@@ -13,3 +13,52 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the employee_db database.')
 );
+
+const startScreenPrompt = () => {
+    return inquirer.prompt([
+        {
+            type:'list',
+            name:'action',
+            message:'What would you like to do?',
+            choices:[
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ])
+    .then((answers) => {
+        
+        switch(answers.action) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                db.end();
+                break;
+        }
+    })
+}
